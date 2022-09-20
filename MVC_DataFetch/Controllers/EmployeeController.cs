ca024aabee9efad58f0d840c7457d6775219ad21
9student129 <9student129@9CMPICA-129>
2022-09-20 15:35:12 +0530

    Add project files.

---
+++ MVC_DataFetch/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using MVC_DataFetch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_DataFetch.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        DBEmployeeEntities context = new DBEmployeeEntities();
+        public ActionResult GetAllEmployees()
+        {
+            List<TBLEmployee> employees = context.TBLEmployees.ToList();
+            return View("EmployeeGet", employees);
+        }
+
+    }
+}
---
+++ MVC_DataFetch/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_DataFetch.MvcApplication" Language="C#" %>
